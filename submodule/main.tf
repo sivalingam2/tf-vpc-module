6d66298491538de700a8a4161be58e079720f775
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2023-12-06 22:05:50 +0530

    create subnets

--- submodule/main.tf
+++ submodule/main.tf
@@ -6,13 +6,16 @@ resource "aws_subnet" "main" {
   tags = {
     Name = each.key
   }
-
 }
-resource "aws_route_table" "example" {
+resource "aws_route_table" "main" {
   for_each = var.subnets
   vpc_id = var.vpc_id
-
   tags = {
     Name = each.key
   }
+}
+resource "aws_route_table_association" "main" {
+  for_each = var.subnets
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.main.id
 }
