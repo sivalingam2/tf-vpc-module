6acc26c25540586e3d901f63250c55262a6e1536
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2023-12-06 21:07:57 +0530

    create vpc

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
 resource "aws_vpc" "main" {
   cidr_block = var.cidr
 }
+resource "aws_subnet" "main" {
+  for_each = var.subnets
+  vpc_id     = aws_vpc.main.id
+  cidr_block = each.value["subnets"]
+
+}
