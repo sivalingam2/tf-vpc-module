4893a1bb07b73993634ef41058cbccd1e78a9c67
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2023-12-09 15:03:24 +0530

    creating nat gate way

--- output.tf
+++ output.tf
@@ -1,6 +1,9 @@
-#output "subnet" {
-#  value = module.subnets
-#}
+output "subnets" {
+  value = module.subnets
+}
 output "public_subnets_ids" {
   value = local.public_subnets_ids
+}
+output "vpc_id" {
+  value = aws_vpc.main.id
 }
