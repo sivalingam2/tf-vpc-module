977a373e9df926d54464b01a2b1d98b7bd5b2c92
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2023-12-09 16:04:22 +0530

    creating nat gate way

--- main.tf
+++ main.tf
@@ -60,41 +60,6 @@ resource "aws_route" "default_vpc_peer_entry" {
   destination_cidr_block    = var.cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
 }
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.main.id
 
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "allow_tls"
-  }
-}
-resource "aws_instance" "instance" {
-  ami           = "ami-03265a0778a880afb"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [ aws_security_group.allow_tls.id]
-  subnet_id = local.app_subnets_ids[0]
-
-  tags = {
-    Name = "default"
-  }
-}
 
 
